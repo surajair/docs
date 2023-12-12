403bdfcc35a2967e492dfcf3a5463029469df2ff
Me <suraj@chaibuilder.com>
2023-12-12 23:02:21 +0530

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,27 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'Chai Builder',
+			social: {
+				github: 'https://github.com/chaibuilder',
+			},
+			sidebar: [
+				{
+					label: 'Getting Started',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', link: '/guides/example/' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
+});
